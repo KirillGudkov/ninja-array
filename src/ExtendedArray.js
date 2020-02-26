2968148daf3dfe53564d70b76ff7091a8e22952f
kirillgudkov <for.example.nickname@gmail.com>
2020-02-26 14:36:04 +0300

    stub at not implemented functions

--- src/ExtendedArray.js
+++ src/ExtendedArray.js
@@ -1,16 +1,12 @@
-Object.defineProperty(Array.prototype, 'get', {
-  value: function(i) {
-    return this[i];
+Object.defineProperty(Array.prototype, 'last', {
+  get: function last() {
+    return this[this.length - 1];
   }
 });
 
-Object.defineProperty(Array.prototype, 'remove', {
-  value: function(...indices) {
-    indices.sort().forEach((index, current) => {
-      this.splice(index - current, 1);
-    });
-
-    return this;
+Object.defineProperty(Array.prototype, 'first', {
+  get: function first() {
+    return this[0];
   }
 });
 
@@ -26,14 +22,66 @@ Object.defineProperty(Array.prototype, 'isNotEmpty', {
   }
 });
 
-Object.defineProperty(Array.prototype, 'last', {
-  get: function last() {
-    return this[this.length - 1];
+Object.defineProperty(Array.prototype, 'get', {
+  value: function(i) {
+    return this[i];
   }
 });
 
-Object.defineProperty(Array.prototype, 'first', {
-  get: function first() {
-    return this[0];
+Object.defineProperty(Array.prototype, 'clear', {
+  value: function() {
+    this.length = 0;
+  }
+});
+
+Object.defineProperty(Array.prototype, 'remove', {
+  value: function(...indices) {
+    indices.sort().forEach((index, current) => {
+      this.splice(index - current, 1);
+    });
+
+    return this;
+  }
+});
+
+Object.defineProperty(Array.prototype, 'removeLast', {
+  value: function(count = 1) {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'removeFirst', {
+  value: function(count = 1) {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'popLast', {
+  value: function() {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'popFirst', {
+  value: function() {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'swap', {
+  value: function(a, b) {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'contains', {
+  value: function(element) {
+    // todo
+  }
+});
+
+Object.defineProperty(Array.prototype, 'containsAll', {
+  value: function(...elements) {
+    // todo
   }
 });
